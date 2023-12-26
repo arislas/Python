6969b8b9501e22f5cf43ec9910123e26432c6329
xto sarcior <arislas@gmail.com>
2023-12-26 17:00:30 +0100

    202312161700

---
+++ 05/05_09_No_Users.py
@@ -0,0 +1,7 @@
+users = ['xto', 'arislas', 'yoston', 'sarcior', 'admin']
+
+for user in users:
+    if user == 'admin':
+        print(f"Hello {user.title()}, would you like to see a status report?")
+    else:
+        print(f"Hello {user.title()}, thank you for logging in again.")
