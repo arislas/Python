c17cff085366bf0ad747f55e38d0f379a627a132
xto sarcior <arislas@gmail.com>
2024-01-01 17:33:57 +0100

    202401011734

--- 09/09_15_Datos_Sorteo.py
+++ 09/09_15_Datos_Sorteo.py
@@ -5,4 +5,10 @@ import loteria
 sorteo = loteria.Loteria((2,5,3,1,4))
 
 print(f"\nMi número es: {sorteo.mi_num()}")
-sorteo.comprueba_numero()
+
+n = 200
+media = sorteo.media_intentos(n)
+
+print(f"\nLa media de intentos de {n} sorteos es: {media}\n")
+coste = 20 * media
+print(f"El coste medio para acertar el número es {coste}\n")
--- 09/loteria.py
+++ 09/loteria.py
@@ -12,14 +12,14 @@ class Loteria:
         self.num_comprado_str = ''
 
     def mi_num(self):
-        """Devuelve una cadena con el número comprado."""
+        """Devuelve un string con el número comprado."""
         self.num_comprado_str = ''
         for num in self.num_comprado:
             self.num_comprado_str += str(num)
         return self.num_comprado_str
     
     def genera_numero(self):
-        """Genera un número de 5 cifras."""
+        """Genera un número de 5 cifras y lo devuelve como string."""
         n = 0
         lista = []
         
@@ -31,9 +31,53 @@ class Loteria:
         return cadena
 
     def comprueba_numero(self):
+        """
+        Simula el sorteo y compara el numero premiado con el nuestro.
+        Devuelve True si son iguales.
+        """
         sorteo = self.genera_numero()
-        print(f"\n{self.mi_num()} == {sorteo} -> {self.mi_num == sorteo}\n")
-        if self.mi_num() == self.genera_numero():
-            return True
+        #print(f"\n{self.mi_num()} == {sorteo} -> {self.mi_num == sorteo}\n")
+        if self.mi_num() == sorteo:
+            return (True, sorteo, self.mi_num())
         else:
-            return False
+            return (False, sorteo, self.mi_num())
+    
+    def n_sorteos(self, n):
+        """Hace n sorteos y comprueba los resultados con nuestro número."""
+        realizados = 0
+        
+        while realizados < n:
+            self.comprueba_numero()
+            realizados += 1
+    
+    def juega_hasta_acertar(self):
+        """saca números hasta que acierta con el nuestro."""
+        intentos = 0
+        
+        while True:
+            intentos += 1
+            resultado = self.comprueba_numero()
+            if resultado[0]:
+                print(f"\n{resultado[2]} == {resultado[1]} -> {resultado[2] == resultado[1]}\n")
+                print(f"\nHas necesitado {intentos} intentos para acertar.\n")
+                return intentos
+                break
+            else:
+                print(f"\n{resultado[2]} == {resultado[1]} -> {resultado[2] == resultado[1]}\n")
+    
+    def media_intentos(self, n):
+        """Devuelve la media para acertar de n sorteos."""
+        sorteos = 0
+        resultados = []
+
+        while(sorteos < n):
+            resultados.append(self.juega_hasta_acertar())
+            sorteos += 1
+        
+        total = 0
+        
+        for resultado in resultados:
+            total += resultado
+        
+        print(resultados)
+        return total / len(resultados)
